511b5a0c8e074e0fd9bb7971ef70b567965c498f
Fazry <lhuqita.fazry@gmail.com>
2022-12-10 02:35:32 +0700

    fixing bug

--- configs/_base_/schedules/schedule_50k.py
+++ configs/_base_/schedules/schedule_50k.py
@@ -5,5 +5,5 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-5, by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=50000)
-checkpoint_config = dict(by_epoch=False, interval=5000)
+checkpoint_config = dict(by_epoch=False, interval=2000)
 evaluation = dict(interval=2000, metric=['mFscore', 'mIoU'], pre_eval=True, save_best='Fscore.changed', greater_keys=['Fscore'])
