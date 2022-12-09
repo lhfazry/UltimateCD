a26ced5166a5656cbd5f1e04f21c1b127333d5ee
Fazry <lhuqita.fazry@gmail.com>
2022-12-09 21:42:01 +0700

    fixing bug

--- configs/wavevit/siam_upernet_wavevit_s_128x128_50k_cdd.py
+++ configs/wavevit/siam_upernet_wavevit_s_128x128_50k_cdd.py
@@ -34,8 +34,8 @@ lr_config = dict(_delete_=True, policy='poly',
                  power=1.0, min_lr=0.0, by_epoch=False)
 
 data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=4)
+    samples_per_gpu=32,
+    workers_per_gpu=8)
 # By default, models are trained on 8 GPUs with 2 images per GPU
 #data=dict(samples_per_gpu=2)
 
