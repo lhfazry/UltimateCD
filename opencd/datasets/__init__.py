1ba7b1197fd8ee97a4ac1467d23480a1b600f4a0
Fazry <lhuqita.fazry@gmail.com>
2022-12-09 21:34:47 +0700

    fixing bug

--- opencd/datasets/__init__.py
+++ opencd/datasets/__init__.py
@@ -2,5 +2,6 @@ from .custom import CDDataset
 from .levir_cd import LEVIR_CD_Dataset
 from .s2looking import S2Looking_Dataset
 from .svcd import SVCD_Dataset
+from .cdd import CDD_Dataset
 
-__all__ = ['CDDataset', 'LEVIR_CD_Dataset', 'S2Looking_Dataset', 'SVCD_Dataset']
+__all__ = ['CDDataset', 'LEVIR_CD_Dataset', 'S2Looking_Dataset', 'SVCD_Dataset', 'CDD_Dataset']
