165074605fcfc497df2c0b78193eee8985ed6001
Fazry <lhuqita.fazry@gmail.com>
2022-12-13 08:55:42 +0700

    add focalnet

--- opencd/models/backbones/focalnet.py
+++ opencd/models/backbones/focalnet.py
@@ -13,9 +13,9 @@ import torch.utils.checkpoint as checkpoint
 import numpy as np
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
-from mmcv_custom import load_checkpoint
-from mmdet.utils import get_root_logger
-from ..builder import BACKBONES
+from mmcv.utils import get_root_logger
+from mmcv.runner import load_checkpoint
+from mmseg.models.builder import BACKBONES
 
 class Mlp(nn.Module):
     """ Multilayer perceptron."""
