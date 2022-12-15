6e90c267921ddfdb224149df424e3eee7a042b54
Fazry <lhuqita.fazry@gmail.com>
2022-12-15 08:06:02 +0700

    add wavecd base

--- configs/wavecd/wavecd_b_256x256_25k_pmd_concat_cdd.py
+++ configs/wavecd/wavecd_b_256x256_25k_pmd_concat_cdd.py
@@ -8,7 +8,7 @@ embed_dims=[64, 128, 320, 512]
 model = dict(
     backbone=dict(
         init_cfg = dict(type='Pretrained', checkpoint='./pretrained/wavevit_b.pth'),
-        stem_hidden_dim=32, 
+        stem_hidden_dim=64, 
         embed_dims=embed_dims,
         num_heads=[2, 4, 10, 16], 
         drop_path_rate=0.3, #0.2, 
