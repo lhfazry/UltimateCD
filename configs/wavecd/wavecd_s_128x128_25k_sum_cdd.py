4b425aa6e86a5665d498129ef49ed3a3c0b3d465
Fazry <lhuqita.fazry@gmail.com>
2022-12-21 17:27:32 +0700

    d

--- configs/wavecd/wavecd_s_128x128_25k_sum_cdd.py
+++ configs/wavecd/wavecd_s_128x128_25k_sum_cdd.py
@@ -48,6 +48,8 @@ data = dict(
     train=dict(pipeline=train_pipeline)
 )
 
+workflow = [('train', 1), ('val', 1)]
+
 # AdamW optimizer, no weight decay for position embedding & layer norm in backbone
 optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
