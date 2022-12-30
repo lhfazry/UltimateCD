8b22ae7dbd69a7ff18456531c46a03267024c7bd
Fazry <lhuqita.fazry@gmail.com>
2022-12-30 15:01:15 +0700

    d

--- tools/clean_checkpoints.sh
+++ tools/clean_checkpoints.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 for file in `find $1 -name "*.pth" -type f`;do 
-    if [[ "$file" =~ .*"20000".* || "$file" =~ .*"25000".* || "$file" =~ .*"50000".* 
-        || "$file" =~ .*"best".* ]]; then
-        echo "Skip $file"
-        continue
+    if [[ "$file" =~ .*"20k".* ]]; then
+        if [[ "$file" =~ .*"20000".* || "$file" =~ .*"best".* ]]; then
+            echo "Skip $file"
+            continue
+        fi
     fi
+
+    if [[ "$file" =~ .*"25k".* ]]; then
+        if [[ "$file" =~ .*"25000".* || "$file" =~ .*"best".* ]]; then
+            echo "Skip $file"
+            continue
+        fi
+    fi
+
+    if [[ "$file" =~ .*"50k".* ]]; then
+        if [[ "$file" =~ .*"50000".*  || "$file" =~ .*"best".*]]; then
+            echo "Skip $file"
+            continue
+        fi
+    fi
+
     echo "delete $file"
 done
-
-#rm iter_1* && rm iter_4* && rm iter_6* && rm iter_8* && rm iter_20* && rm iter_22* && rm iter_24*
