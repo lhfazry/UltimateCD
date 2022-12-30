50af7fbc852af0a0456073097d24b0f673a8ac1a
Fazry <lhuqita.fazry@gmail.com>
2022-12-30 14:55:08 +0700

    d

---
+++ tools/clean_checkpoints.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in `find $1 -name "*.pth" -type f`;do 
+    if [[ "$file" =~ .*"20000".* || "$file" =~ .*"250-0".* || "$file" =~ .*"best".*]]; then
+        echo "Skip $file"
+        continue
+    fi
+    echo "delete $file"
+done
+
+#rm iter_1* && rm iter_4* && rm iter_6* && rm iter_8* && rm iter_20* && rm iter_22* && rm iter_24*
