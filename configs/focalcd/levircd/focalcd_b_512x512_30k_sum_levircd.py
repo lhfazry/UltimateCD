9805b70b1d285d187faae52f76088de578b49390
Fazry <lhuqita.fazry@gmail.com>
2023-01-20 04:30:11 +0900

    add new config

--- configs/focalcd/levircd/focalcd_b_512x512_30k_sum_levircd.py
+++ configs/focalcd/levircd/focalcd_b_512x512_30k_sum_levircd.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../../_base_/models/focalnet/focalnet_base_lrf.py', '../../_base_/datasets/levir_cd.py',
-    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_50k.py'
+    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_30k.py'
 ]
 
 in_channels=[128, 256, 512, 1024]
