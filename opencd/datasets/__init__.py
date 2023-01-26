d3b5c6ff8915f1cc7c4664042ded94899a47526d
Fazry <lhuqita.fazry@gmail.com>
2023-01-26 18:06:03 +0700

    add more config

--- opencd/datasets/__init__.py
+++ opencd/datasets/__init__.py
@@ -3,5 +3,6 @@ from .levir_cd import LEVIR_CD_Dataset
 from .s2looking import S2Looking_Dataset
 from .svcd import SVCD_Dataset
 from .cdd import CDD_Dataset
+from .whu import WHU_Dataset
 
-__all__ = ['CDDataset', 'LEVIR_CD_Dataset', 'S2Looking_Dataset', 'SVCD_Dataset', 'CDD_Dataset']
+__all__ = ['CDDataset', 'LEVIR_CD_Dataset', 'S2Looking_Dataset', 'SVCD_Dataset', 'CDD_Dataset', 'WHU_Dataset']
