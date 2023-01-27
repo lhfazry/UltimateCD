66e09b9eb5d7512329322484b443d69ce5af5b3d
Fazry <lhuqita.fazry@gmail.com>
2023-01-27 08:44:36 +0700

    add new config

---
+++ configs/focalcd/whu/focalcd_s_512x512_30k_pmd_h30_sum_whu.py
@@ -0,0 +1,64 @@
+_base_ = [
+    '../../_base_/models/focalnet/focalnet_small_lrf.py', '../../_base_/datasets/whu.py',
+    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_30k.py'
+]
+
+in_channels = [96, 192, 384, 768]
+
+model = dict(
+    neck=dict(type='FeatureFusionNeck', policy='sum'),
+    decode_head=dict(
+        in_channels=[v for v in in_channels],
+        num_classes=2
+    ),
+    auxiliary_head=dict(
+        in_channels=in_channels[2],
+        num_classes=2
+    ))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+crop_size = (512, 512)
+
+train_pipeline = [
+    dict(type='MultiImgLoadImageFromFile'),
+    dict(type='MultiImgLoadAnnotations'),
+    dict(type='MultiImgRandomCrop', crop_size=crop_size),
+    dict(type='MultiImgRandomRotate', prob=0.5, degree=180),
+    dict(type='MultiImgRandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='MultiImgRandomFlip', prob=0.5, direction='vertical'),
+    dict(type='MultiImgExchangeTime', prob=0.5),
+
+    dict(
+        type='MultiImgPhotoMetricDistortion',
+        hue_delta=30),
+    
+    dict(type='MultiImgNormalize', **img_norm_cfg),
+    dict(type='MultiImgDefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=4,
+    train=dict(pipeline=train_pipeline)
+)
+
+# AdamW optimizer, no weight decay for position embedding & layer norm in backbone
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+#data=dict(samples_per_gpu=2)
+
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+fp16 = dict()
+work_dir = './work_dirs/focalcd/whu/focalcd_s_512x512_30k_pmd_h30_sum_whu'
