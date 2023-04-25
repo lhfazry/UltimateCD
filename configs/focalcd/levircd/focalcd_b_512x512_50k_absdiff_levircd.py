e23fc24beb5668dd2cc9591438a8f6db3f3da3cb
Fazry <lhuqita.fazry@gmail.com>
2023-04-25 15:17:02 +0700

    add new config

--- configs/focalcd/levircd/focalcd_b_512x512_50k_absdiff_levircd.py
+++ configs/focalcd/levircd/focalcd_b_512x512_50k_absdiff_levircd.py
@@ -14,7 +14,8 @@ model = dict(
     auxiliary_head=dict(
         in_channels=in_channels[2],
         num_classes=2
-    ))
+    )
+)
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
