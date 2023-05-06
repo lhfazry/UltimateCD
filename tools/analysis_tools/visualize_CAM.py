e30252ebe7ccf946a24f701d24ef490609896c8e
Fazry <lhuqita.fazry@gmail.com>
2023-05-06 16:49:50 +0700

    add new config

--- tools/analysis_tools/visualize_CAM.py
+++ tools/analysis_tools/visualize_CAM.py
@@ -26,7 +26,7 @@ from PIL import Image
 from pytorch_grad_cam.base_cam import BaseCAM
 from typing import Callable, List, Tuple
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-#from pytorch_grad_cam.activations_and_gradients import ActivationsAndGradients
+from pytorch_grad_cam.activations_and_gradients import ActivationsAndGradients
 from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image, scale_cam_image
 
 
@@ -133,23 +133,9 @@ def logit_simple_test(self, img, img_meta, rescale=True):
     return seg_pred
 
 
-class MMActivationsAndGradients():
-    def __init__(self, model,
-            target_layers,
-            reshape_transform):
-        #super(MMActivationsAndGradients, self).__init__(*args, **kwargs)
-        self.model = model
-        self.gradients = []
-        self.activations = []
-        self.reshape_transform = reshape_transform
-        self.handles = []
-        for target_layer in target_layers:
-            self.handles.append(
-                target_layer.register_forward_hook(self.save_activation))
-            # Because of https://github.com/pytorch/pytorch/issues/61519,
-            # we don't use backward hook to record gradients.
-            self.handles.append(
-                target_layer.register_forward_hook(self.save_gradient))
+class MMActivationsAndGradients(ActivationsAndGradients):
+    def __init__(self, *args, **kwargs):
+        super(MMActivationsAndGradients, self).__init__(*args, **kwargs)
     
     def __call__(self, x):
         
@@ -157,30 +143,13 @@ class MMActivationsAndGradients():
         self.activations = []
         out = self.model(return_loss=False, **x)
         return out
-    
+
     def save_activation(self, module, input, output):
         activation = output
 
         if self.reshape_transform is not None:
             activation = self.reshape_transform(activation)
         self.activations.append(activation[0].cpu().detach())
-
-    def save_gradient(self, module, input, output):
-        if not hasattr(output, "requires_grad") or not output.requires_grad:
-            # You can only register hooks on tensor requires grad.
-            return
-
-        # Gradients are computed in reverse order
-        def _store_grad(grad):
-            if self.reshape_transform is not None:
-                grad = self.reshape_transform(grad)
-            self.gradients = [grad.cpu().detach()] + self.gradients
-
-        output.register_hook(_store_grad)
-
-    def release(self):
-        for handle in self.handles:
-            handle.remove()
 
 class MMGradCAM(BaseCAM):
     def __init__(self, model, target_layers, use_cuda=False,
