510e8dc59b6b84c8197eeb3f375b4ab9d7ab81d6
Fazry <lhuqita.fazry@gmail.com>
2023-05-06 17:30:58 +0700

    fixing bug

--- tools/analysis_tools/visualize_CAM3.py
+++ tools/analysis_tools/visualize_CAM3.py
@@ -134,9 +134,37 @@ def logit_simple_test(self, img, img_meta, rescale=True):
 
 
 class MMActivationsAndGradients(ActivationsAndGradients):
-    def __init__(self, *args, **kwargs):
-        super(MMActivationsAndGradients, self).__init__(*args, **kwargs)
-    
+    def __init__(self, model, target_layers, reshape_transform):
+        self.model = model
+        self.gradients = []
+        self.activations = []
+        self.reshape_transform = reshape_transform
+        self.handles = []
+        for target_layer in target_layers:
+            self.handles.append(
+                target_layer.register_forward_hook(self.save_activation))
+            # Because of https://github.com/pytorch/pytorch/issues/61519,
+            # we don't use backward hook to record gradients.
+            self.handles.append(
+                target_layer.register_forward_hook(self.save_gradient))
+
+    def save_gradient(self, module, input, output):
+        if not hasattr(output, "requires_grad") or not output.requires_grad:
+            # You can only register hooks on tensor requires grad.
+            return
+
+        # Gradients are computed in reverse order
+        def _store_grad(grad):
+            if self.reshape_transform is not None:
+                grad = self.reshape_transform(grad)
+            self.gradients = [grad.cpu().detach()] + self.gradients
+
+        output.register_hook(_store_grad)
+
+    def release(self):
+        for handle in self.handles:
+            handle.remove()
+
     def __call__(self, x):
         
         self.gradients = []
