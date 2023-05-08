fab03088d0b0a68672eea99c3ad8a3e6e79742e0
Fazry <lhuqita.fazry@gmail.com>
2023-05-09 06:50:44 +0700

    fix bug

--- tools/analysis_tools/BaseCAM.py
+++ tools/analysis_tools/BaseCAM.py
@@ -52,7 +52,10 @@ class BaseCAM:
                                        targets,
                                        activations,
                                        grads)
+        print(f"weights: {weights.shape}")
         weighted_activations = weights[:, :, None, None] * activations
+        print(f"weighted_activations: {weighted_activations.shape}")
+        
         if eigen_smooth:
             cam = get_2d_projection(weighted_activations)
         else:
