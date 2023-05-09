f0b99ffef3a0be05f0f22134f56d28b1fcd9e7c6
Fazry <lhuqita.fazry@gmail.com>
2023-05-09 14:44:24 +0700

    fix bug

--- tools/analysis_tools/visualize_CAM2.py
+++ tools/analysis_tools/visualize_CAM2.py
@@ -150,10 +150,10 @@ class MMActivationsAndGradient():
                 target_layer.register_forward_hook(self.save_gradient))
 
     def save_gradient(self, module, input, output):
-        print(output)
-        if not hasattr(output, "requires_grad") or not output.requires_grad:
+        #print(output)
+        #if not hasattr(output, "requires_grad") or not output.requires_grad:
             # You can only register hooks on tensor requires grad.
-            return
+        #    return
 
         # Gradients are computed in reverse order
         def _store_grad(grad):
