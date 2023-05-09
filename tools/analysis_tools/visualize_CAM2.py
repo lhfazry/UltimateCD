5d458c059ca9cb2547506066165aff44a9ba2a42
Fazry <lhuqita.fazry@gmail.com>
2023-05-09 09:46:52 +0700

    fix bug

--- tools/analysis_tools/visualize_CAM2.py
+++ tools/analysis_tools/visualize_CAM2.py
@@ -483,7 +483,7 @@ def main():
         res = res.argmax(dim=0).float().cpu().numpy()
         
         # change the `target_layers` here
-        target_layers = [model.module.backbone.layers0]
+        target_layers = [model.module.backbone.layers[0]]
         # target_layers = [model.module.decode_head.discriminator]
         targets = [SemanticSegmentationTarget(1, res)]
         
