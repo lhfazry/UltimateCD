6adf42f37d3ee30191e84e0364f55dc2b2c4da39
Fazry <lhuqita.fazry@gmail.com>
2023-05-11 16:27:39 +0700

    add swinsunet

--- opencd/models/necks/swin_fusion.py
+++ opencd/models/necks/swin_fusion.py
@@ -25,7 +25,6 @@ class SwinFusionNeck(BaseModule):
     @auto_fp16()
     def forward(self, x1, x2):
         """Forward function."""
-        print(f"x1: {x1.shape}, x2: {x2.shape}")
 
         assert len(x1) == len(x2), "The features x1 and x2 from the" \
             "backbone should be of equal shape"
@@ -33,6 +32,7 @@ class SwinFusionNeck(BaseModule):
         outs = []
         for i in range(len(x1)):
             out = self.fusion(x1[i], x2[i])
+            print(f"x1: {x1[i].shape}, x2: {x2[i].shape}, out: {out.shape}")
 
             if i == len(x1) - 1:    
                 out = self.projection(out)
