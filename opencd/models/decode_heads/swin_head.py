1ccd9442aedc94b3e2bd0740486283cbef1c80eb
Fazry <lhuqita.fazry@gmail.com>
2023-05-11 12:43:09 +0700

    add swinsunet

--- opencd/models/decode_heads/swin_head.py
+++ opencd/models/decode_heads/swin_head.py
@@ -435,7 +435,6 @@ class BasicUpLayer(nn.Module):
     def __init__(self, dim, input_resolution, depth, num_heads, window_size,
                  mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., norm_layer=nn.LayerNorm, upsample=None, use_checkpoint=False):
-
         super().__init__()
         self.dim = dim
         self.input_resolution = input_resolution
@@ -445,13 +444,13 @@ class BasicUpLayer(nn.Module):
         # build blocks
         self.blocks = nn.ModuleList([
             SwinTransformerBlock(
-                dim=dim, 
+                dim=dim,
                 input_resolution=input_resolution,
-                num_heads=num_heads, 
+                num_heads=num_heads,
                 window_size=window_size,
                 shift_size=0 if (i % 2 == 0) else window_size // 2,
                 mlp_ratio=mlp_ratio,
-                qkv_bias=qkv_bias, 
+                qkv_bias=qkv_bias,
                 qk_scale=qk_scale,
                 drop=drop, 
                 attn_drop=attn_drop,
@@ -504,26 +503,26 @@ class SwinHead(BaseDecodeHead):
     """
 
     def __init__(self, 
-                 img_size=224, 
-                 patch_size=4, 
-                 in_chans=3, 
-                 num_classes=1000,
-                 embed_dim=96, 
-                 depths=[2, 2, 6, 2], 
-                 num_heads=[3, 6, 12, 24],
-                 window_size=7, 
-                 mlp_ratio=4., 
-                 qkv_bias=True, 
-                 qk_scale=None,
-                 drop_rate=0., 
-                 attn_drop_rate=0., 
-                 drop_path_rate=0.1,
-                 norm_layer=nn.LayerNorm, 
-                 ape=False, patch_norm=True,
-                 use_checkpoint=False, 
-                 final_upsample="expand_first",
-                 fused_window_process=False, 
-                 **kwargs):
+            img_size=224, 
+            patch_size=4, 
+            in_chans=3, 
+            num_classes=1000,
+            embed_dim=96, 
+            depths=[2, 2, 6, 2], 
+            num_heads=[3, 6, 12, 24],
+            window_size=7, 
+            mlp_ratio=4., 
+            qkv_bias=True, 
+            qk_scale=None,
+            drop_rate=0., 
+            attn_drop_rate=0., 
+            drop_path_rate=0.1,
+            norm_layer=nn.LayerNorm, 
+            ape=False, patch_norm=True,
+            use_checkpoint=False, 
+            final_upsample="expand_first",
+            fused_window_process=False, 
+            **kwargs):
         super().__init__(num_classes=num_classes, **kwargs)
 
         self.num_classes = num_classes
