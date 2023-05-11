2bd4bf529cd13b605c36e2bb6b39ad1feeb069cb
Fazry <lhuqita.fazry@gmail.com>
2023-05-11 11:47:07 +0700

    add swinsunet

--- opencd/models/necks/__init__.py
+++ opencd/models/necks/__init__.py
@@ -1,5 +1,5 @@
 from .feature_fusion import FeatureFusionNeck
 from .mix_attention_neck import MixAttentionNeck
-#from .swin_fusion import SwinFusionNeck
+from .swin_fusion import SwinFusionNeck
 
-__all__ = ['FeatureFusionNeck', 'MixAttentionNeck']
+__all__ = ['FeatureFusionNeck', 'MixAttentionNeck', 'SwinFusionNeck']
