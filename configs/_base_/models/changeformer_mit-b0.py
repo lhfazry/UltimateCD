93fccff377e5689fe758d79f33262d090b513c41
Fazry <lhuqita.fazry@gmail.com>
2023-05-12 15:09:52 +0700

    fixing bugs

--- configs/_base_/models/changeformer_mit-b0.py
+++ configs/_base_/models/changeformer_mit-b0.py
@@ -1,17 +1,17 @@
 # model settings
 norm_cfg = dict(type='SyncBN', requires_grad=True)
-data_preprocessor = dict(
-    type='DualInputSegDataPreProcessor',
-    mean=[123.675, 116.28, 103.53] * 2,
-    std=[58.395, 57.12, 57.375] * 2,
-    bgr_to_rgb=True,
-    size_divisor=32,
-    pad_val=0,
-    seg_pad_val=255,
-    test_cfg=dict(size_divisor=32))
+#data_preprocessor = dict(
+#    type='DualInputSegDataPreProcessor',
+#    mean=[123.675, 116.28, 103.53] * 2,
+#    std=[58.395, 57.12, 57.375] * 2,
+#    bgr_to_rgb=True,
+#    size_divisor=32,
+#    pad_val=0,
+#    seg_pad_val=255,
+#    test_cfg=dict(size_divisor=32))
 model = dict(
     type='SiamEncoderDecoder',
-    data_preprocessor=data_preprocessor,
+    #data_preprocessor=data_preprocessor,
     pretrained=None,
     backbone=dict(
         type='MixVisionTransformer',
