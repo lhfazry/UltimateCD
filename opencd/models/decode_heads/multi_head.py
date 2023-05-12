b8fe44ac1fb02a75434a93c5a06fd9bfc3eff0ad
Fazry <lhuqita.fazry@gmail.com>
2023-05-12 14:45:14 +0700

    fixing bugs

--- opencd/models/decode_heads/multi_head.py
+++ opencd/models/decode_heads/multi_head.py
@@ -13,7 +13,32 @@ from mmseg.ops import resize
 from mmseg.utils import ConfigType, SampleList, add_prefix
 #from opencd.registry import MODELS
 from mmseg.models.builder import HEADS
+from addict import Dict
+from typing import Dict, List, Optional, Sequence, Tuple, Union
 
+class ConfigDict(Dict):
+    """A dictionary for config which has the same interface as python's built-
+    in dictionary and can be used as a normal dictionary.
+
+    The Config class would transform the nested fields (dictionary-like fields)
+    in config file into ``ConfigDict``.
+    """
+
+    def __missing__(self, name):
+        raise KeyError(name)
+
+    def __getattr__(self, name):
+        try:
+            value = super().__getattr__(name)
+        except KeyError:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no "
+                                 f"attribute '{name}'")
+        except Exception as e:
+            raise e
+        else:
+            return value
+        
+ConfigType = Union[ConfigDict, dict]
 
 @HEADS.register_module()
 class MultiHeadDecoder(BaseDecodeHead):
