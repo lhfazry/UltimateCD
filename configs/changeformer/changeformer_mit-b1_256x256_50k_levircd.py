708f3ef32ed0c05a2a16fc6c2c85e3cd89778ca8
Fazry <lhuqita.fazry@gmail.com>
2023-05-12 15:06:54 +0700

    fixing bugs

---
+++ configs/changeformer/changeformer_mit-b1_256x256_50k_levircd.py
@@ -0,0 +1,54 @@
+_base_ = ['../_base_/models/changeformer_mit-b0_256x256_40k_levircd.py', '../_base_/datasets/levir_cd.py',
+        '../_base_/default_runtime.py', '../_base_/schedules/schedule_50k.py']
+
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b1_20220624-02e5a6a1.pth'  # noqa
+
+# model settings
+model = dict(
+    pretrained=checkpoint,
+    backbone=dict(
+        embed_dims=64, num_heads=[1, 2, 5, 8], num_layers=[2, 2, 2, 2]),
+    decode_head=dict(in_channels=[v * 2 for v in [64, 128, 320, 512]]))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+crop_size = (512, 512)
+
+train_pipeline = [
+    dict(type='MultiImgLoadImageFromFile'),
+    dict(type='MultiImgLoadAnnotations'),
+    dict(type='MultiImgRandomCrop', crop_size=crop_size),
+    dict(type='MultiImgRandomRotate', prob=0.5, degree=180),
+    dict(type='MultiImgRandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='MultiImgRandomFlip', prob=0.5, direction='vertical'),
+    dict(type='MultiImgExchangeTime', prob=0.5),
+    
+    dict(type='MultiImgNormalize', **img_norm_cfg),
+    dict(type='MultiImgDefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=4,
+    train=dict(pipeline=train_pipeline)
+)
+
+# AdamW optimizer, no weight decay for position embedding & layer norm in backbone
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+#data=dict(samples_per_gpu=2)
+
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+fp16 = dict()
+work_dir = './work_dirs/changerformer/levircd/changeformer_mit-b1_256x256_50k_levircd'
