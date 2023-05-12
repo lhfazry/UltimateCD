4433db40102f8c5ac82b2e78213a58437ea34ffc
Fazry <lhuqita.fazry@gmail.com>
2023-05-12 15:46:02 +0700

    fixing bugs

--- configs/changeformer/changeformer_mit-b1_256x256_50k_levircd.py
+++ configs/changeformer/changeformer_mit-b1_256x256_50k_levircd.py
@@ -34,17 +34,20 @@ data = dict(
     train=dict(pipeline=train_pipeline)
 )
 
-optimizer=dict(
-    type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01)
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
 
-optim_wrapper = dict(
-    _delete_=True,
-    type='OptimWrapper',
-    optimizer=optimizer,
-    paramwise_cfg=dict(
-        custom_keys={
-            'pos_block': dict(decay_mult=0.),
-            'norm': dict(decay_mult=0.),
-            'head': dict(lr_mult=10.)
-        }))
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+#data=dict(samples_per_gpu=2)
+
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+fp16 = dict()
 work_dir = './work_dirs/changerformer/levircd/changeformer_mit-b1_256x256_50k_levircd'
