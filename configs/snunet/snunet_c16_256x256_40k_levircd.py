1035ed05872144d56d4ca9d47cb83f37cea491ed
Fazry <lhuqita.fazry@gmail.com>
2023-05-12 17:38:56 +0700

    fixing bugs

--- configs/snunet/snunet_c16_256x256_40k_levircd.py
+++
@@ -1,3 +0,0 @@
-_base_ = [
-    '../_base_/models/snunet_c16.py',
-    '../common/standard_256x256_40k_levircd.py']
---
+++ configs/snunet/snunet_c16_256x256_50k_levircd.py
@@ -0,0 +1,45 @@
+_base_ = ['../_base_/models/snunet_c16.py', '../_base_/datasets/levir_cd.py',
+        '../_base_/default_runtime.py', '../_base_/schedules/schedule_50k.py']
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+crop_size = (512, 512)
+
+train_pipeline = [
+    dict(type='MultiImgLoadImageFromFile'),
+    dict(type='MultiImgLoadAnnotations'),
+    dict(type='MultiImgRandomCrop', crop_size=crop_size),
+    dict(type='MultiImgRandomRotate', prob=0.5, degree=180),
+    dict(type='MultiImgRandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='MultiImgRandomFlip', prob=0.5, direction='vertical'),
+    dict(type='MultiImgExchangeTime', prob=0.5),
+    
+    dict(type='MultiImgNormalize', **img_norm_cfg),
+    dict(type='MultiImgDefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=4,
+    train=dict(pipeline=train_pipeline)
+)
+
+optimizer = dict(_delete_=True, type='AdamW', lr=0.001, betas=(0.9, 0.999), weight_decay=0.05,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+
+lr_config = dict(_delete_=True, 
+                 policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+#data=dict(samples_per_gpu=2)
+
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+fp16 = dict()
+work_dir = './work_dirs/snunet/snunet_c16_256x256_50k_levircd'
