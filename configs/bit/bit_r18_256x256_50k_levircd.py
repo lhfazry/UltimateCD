d59b017c536c0e5941fe257da0997ae0211e7399
Fazry <lhuqita.fazry@gmail.com>
2023-05-13 03:28:06 +0700

    fixing bug

--- configs/bit/bit_r18_256x256_50k_levircd.py
+++ configs/bit/bit_r18_256x256_50k_levircd.py
@@ -21,7 +21,7 @@ crop_size = (256, 256)
 train_pipeline = [
     dict(type='MultiImgLoadImageFromFile'),
     dict(type='MultiImgLoadAnnotations'),
-    dict(type='MultiImgResize', img_scale=0.5),
+    dict(type='MultiImgResize', img_scale=(512, 512)),
     dict(type='MultiImgRandomCrop', crop_size=crop_size),
     dict(type='MultiImgRandomRotate', prob=0.5, degree=180),
     dict(type='MultiImgRandomFlip', prob=0.5, direction='horizontal'),
@@ -32,11 +32,28 @@ train_pipeline = [
     dict(type='MultiImgDefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
+test_pipeline = [
+    dict(type='MultiImgLoadImageFromFile'),
+    dict(
+        type='MultiImgMultiScaleFlipAug',
+        img_scale=(512, 512),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='MultiImgResize', keep_ratio=True),
+            dict(type='MultiImgRandomFlip'),
+            dict(type='MultiImgNormalize', **img_norm_cfg),
+            dict(type='MultiImgImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
 
 data = dict(
-    samples_per_gpu=64,
+    samples_per_gpu=16,
     workers_per_gpu=4,
-    train=dict(pipeline=train_pipeline)
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline)
 )
 
 # optimizer
