ae20214b774376fa75ba96f555f25ff9763592f6
Fazry <lhuqita.fazry@gmail.com>
2023-05-17 20:17:13 +0700

    add new config

---
+++ configs/changeformer/changeformer_mit-b1_256x256_50k_diff_whu.py
@@ -0,0 +1,30 @@
+_base_ = ['../_base_/models/changeformer_mit-b0.py', '../_base_/datasets/whu256.py',
+        '../_base_/default_runtime.py', '../_base_/schedules/schedule_50k.py']
+
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b1_20220624-02e5a6a1.pth'  # noqa
+
+# model settings
+model = dict(
+    #pretrained=checkpoint,
+    backbone=dict(
+        embed_dims=64, num_heads=[1, 2, 5, 8], num_layers=[2, 2, 2, 2]),
+    neck=dict(type='FeatureFusionNeck', policy='diff'),
+    decode_head=dict(in_channels=[v * 2 for v in [64, 128, 320, 512]]))
+
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+#data=dict(samples_per_gpu=2)
+
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+fp16 = dict()
+work_dir = './work_dirs/changerformer/changeformer_mit-b1_256x256_50k_whu'
