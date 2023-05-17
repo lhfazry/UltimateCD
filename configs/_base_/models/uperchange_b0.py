fe6a96a0664d8817e3144c70643db6fb47a6e5ba
Fazry <lhuqita.fazry@gmail.com>
2023-05-17 21:35:39 +0700

    add new config

---
+++ configs/_base_/models/uperchange_b0.py
@@ -0,0 +1,46 @@
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+#data_preprocessor = dict(
+#    type='DualInputSegDataPreProcessor',
+#    mean=[123.675, 116.28, 103.53] * 2,
+#    std=[58.395, 57.12, 57.375] * 2,
+#    bgr_to_rgb=True,
+#    size_divisor=32,
+#    pad_val=0,
+#    seg_pad_val=255,
+#    test_cfg=dict(size_divisor=32))
+model = dict(
+    type='SiamEncoderDecoder',
+    #data_preprocessor=data_preprocessor,
+    pretrained=None,
+    backbone=dict(
+        type='MixVisionTransformer',
+        in_channels=3,
+        embed_dims=32,
+        num_stages=4,
+        num_layers=[2, 2, 2, 2],
+        num_heads=[1, 2, 5, 8],
+        patch_sizes=[7, 3, 3, 3],
+        sr_ratios=[8, 4, 2, 1],
+        out_indices=(0, 1, 2, 3),
+        mlp_ratio=4,
+        qkv_bias=True,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.1),
+    neck=dict(type='FeatureFusionNeck', policy='concat'),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[v * 2 for v in [32, 64, 160, 256]],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=128,
+        dropout_ratio=0.1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
