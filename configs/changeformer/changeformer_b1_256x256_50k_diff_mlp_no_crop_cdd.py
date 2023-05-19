0e960313f46df5d0d963137eeb74958c1c6b2d5b
Fazry <lhuqita.fazry@gmail.com>
2023-05-19 20:38:17 +0700

    add new config

---
+++ configs/changeformer/changeformer_b1_256x256_50k_diff_mlp_no_crop_cdd.py
@@ -0,0 +1,23 @@
+_base_ = ['../_base_/models/changeformer/changeformer_b1.py', '../_base_/datasets/cdd256.py',
+        '../_base_/default_runtime.py', '../_base_/schedules/schedule_50k.py']
+
+model = dict(
+    decode_head=dict(type='SegFormerMLPHead'))
+
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+#data=dict(samples_per_gpu=2)
+
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+fp16 = dict()
+work_dir = './work_dirs/changeformer/changeformer_b1_256x256_50k_diff_mlp_no_crop_cdd.py'
