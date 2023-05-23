0474b4d5dd0f36d750ecf598e6fc81517284fa27
Fazry <lhuqita.fazry@gmail.com>
2023-05-23 19:36:58 +0700

    add new config

--- opencd/models/backbones/local_mix_vit.py
+++ opencd/models/backbones/local_mix_vit.py
@@ -15,6 +15,123 @@ from mmcv.runner import BaseModule, ModuleList, Sequential
 from ..builder import BACKBONES
 from ..utils import PatchEmbed, nchw_to_nlc, nlc_to_nchw
 
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class ECALayer(nn.Module):
+    def __init__(self, channel, gamma=2, b=1, sigmoid=True):
+        super(ECALayer, self).__init__()
+        t = int(abs((math.log(channel, 2) + b) / gamma))
+        k = t if t % 2 else t + 1
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k, padding=k // 2, bias=False)
+        if sigmoid:
+            self.sigmoid = nn.Sigmoid()
+        else:
+            self.sigmoid = h_sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2))
+        y = y.transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=4):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                h_sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+class LocalityFeedForward(nn.Module):
+    def __init__(self, in_dim, out_dim, stride, expand_ratio=4., act='hs+se', reduction=4,
+                 wo_dp_conv=False, dp_first=False):
+        """
+        :param in_dim: the input dimension
+        :param out_dim: the output dimension. The input and output dimension should be the same.
+        :param stride: stride of the depth-wise convolution.
+        :param expand_ratio: expansion ratio of the hidden dimension.
+        :param act: the activation function.
+                    relu: ReLU
+                    hs: h_swish
+                    hs+se: h_swish and SE module
+                    hs+eca: h_swish and ECA module
+                    hs+ecah: h_swish and ECA module. Compared with eca, h_sigmoid is used.
+        :param reduction: reduction rate in SE module.
+        :param wo_dp_conv: without depth-wise convolution.
+        :param dp_first: place depth-wise convolution as the first layer.
+        """
+        super(LocalityFeedForward, self).__init__()
+        hidden_dim = int(in_dim * expand_ratio)
+        kernel_size = 3
+
+        layers = []
+        # the first linear layer is replaced by 1x1 convolution.
+        layers.extend([
+            nn.Conv2d(in_dim, hidden_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            h_swish() if act.find('hs') >= 0 else nn.ReLU6(inplace=True)])
+
+        # the depth-wise convolution between the two linear layers
+        if not wo_dp_conv:
+            dp = [
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, kernel_size // 2, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                h_swish() if act.find('hs') >= 0 else nn.ReLU6(inplace=True)
+            ]
+            if dp_first:
+                layers = dp + layers
+            else:
+                layers.extend(dp)
+
+        if act.find('+') >= 0:
+            attn = act.split('+')[1]
+            if attn == 'se':
+                layers.append(SELayer(hidden_dim, reduction=reduction))
+            elif attn.find('eca') >= 0:
+                layers.append(ECALayer(hidden_dim, sigmoid=attn == 'eca'))
+            else:
+                raise NotImplementedError('Activation type {} is not implemented'.format(act))
+
+        # the second linear layer is replaced by 1x1 convolution.
+        layers.extend([
+            nn.Conv2d(hidden_dim, out_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(out_dim)
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = x + self.conv(x)
+        return x
 
 class MixFFN(BaseModule):
     """An implementation of MixFFN of Segformer.
