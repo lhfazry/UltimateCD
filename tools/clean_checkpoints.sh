2e43925adcf1f9d92806085240fbd4238c91f08c
Fazry <lhuqita.fazry@gmail.com>
2023-05-27 16:28:11 +0700

    add new config

--- tools/clean_checkpoints.sh
+++ tools/clean_checkpoints.sh
@@ -29,6 +29,12 @@ for file in `find $1 -name "*.pth" -type f`;do
 
     if [[ "$file" =~ .*"$latest_pth".* ]]; then
         echo "Skip latest pth: $latest_pth"
+
+        if [[-f "best_Fscore.changed_$latest_pth"]]; then
+            rm "best_Fscore.changed_$latest_pth"
+            echo "Delete best score latest pth: best_Fscore.changed_$latest_pth"
+        fi
+
         continue
     fi
 
