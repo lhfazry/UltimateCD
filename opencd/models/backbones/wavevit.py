5d7af3664613c04f025b30605747091b4d08c01e
Fazry <lhuqita.fazry@gmail.com>
2023-05-28 05:27:13 +0700

    add new configg

--- opencd/models/backbones/wavevit.py
+++ opencd/models/backbones/wavevit.py
@@ -13,6 +13,124 @@ from mmcv.runner import BaseModule
 from mmcv.runner import load_checkpoint
 from mmseg.utils import get_root_logger
 
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class ECALayer(nn.Module):
+    def __init__(self, channel, gamma=2, b=1, sigmoid=True):
+        super(ECALayer, self).__init__()
+        t = int(abs((math.log(channel, 2) + b) / gamma))
+        k = t if t % 2 else t + 1
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k, padding=k // 2, bias=False)
+        if sigmoid:
+            self.sigmoid = nn.Sigmoid()
+        else:
+            self.sigmoid = h_sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2))
+        y = y.transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=4):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                h_sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+class LocalityFeedForward(nn.Module):
+    def __init__(self, in_dim, out_dim, stride, expand_ratio=4., act='hs+se', reduction=4,
+                 wo_dp_conv=False, dp_first=False):
+        """
+        :param in_dim: the input dimension
+        :param out_dim: the output dimension. The input and output dimension should be the same.
+        :param stride: stride of the depth-wise convolution.
+        :param expand_ratio: expansion ratio of the hidden dimension.
+        :param act: the activation function.
+                    relu: ReLU
+                    hs: h_swish
+                    hs+se: h_swish and SE module
+                    hs+eca: h_swish and ECA module
+                    hs+ecah: h_swish and ECA module. Compared with eca, h_sigmoid is used.
+        :param reduction: reduction rate in SE module.
+        :param wo_dp_conv: without depth-wise convolution.
+        :param dp_first: place depth-wise convolution as the first layer.
+        """
+        super(LocalityFeedForward, self).__init__()
+        hidden_dim = int(in_dim * expand_ratio)
+        kernel_size = 3
+
+        layers = []
+        # the first linear layer is replaced by 1x1 convolution.
+        layers.extend([
+            nn.Conv2d(in_dim, hidden_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            h_swish() if act.find('hs') >= 0 else nn.ReLU6(inplace=True)])
+
+        # the depth-wise convolution between the two linear layers
+        if not wo_dp_conv:
+            dp = [
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, kernel_size // 2, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                h_swish() if act.find('hs') >= 0 else nn.ReLU6(inplace=True)
+            ]
+            if dp_first:
+                layers = dp + layers
+            else:
+                layers.extend(dp)
+
+        if act.find('+') >= 0:
+            attn = act.split('+')[1]
+            if attn == 'se':
+                layers.append(SELayer(hidden_dim, reduction=reduction))
+            elif attn.find('eca') >= 0:
+                layers.append(ECALayer(hidden_dim, sigmoid=attn == 'eca'))
+            else:
+                raise NotImplementedError('Activation type {} is not implemented'.format(act))
+
+        # the second linear layer is replaced by 1x1 convolution.
+        layers.extend([
+            nn.Conv2d(hidden_dim, out_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(out_dim)
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = x + self.conv(x)
+        return x
+
 
 class DWT_Function(Function):
     @staticmethod
@@ -282,18 +400,24 @@ class Block(nn.Module):
         norm_layer=nn.LayerNorm, 
         sr_ratio=1, 
         block_type = 'wave',
-        wave='haar'
+        wave='haar',
+        locality_ffn=False
     ):
         super().__init__()
+        self.locality_ffn = locality_ffn
         self.norm1 = norm_layer(dim)
-        self.norm2 = norm_layer(dim)
 
         if block_type == 'std_att':
             self.attn = Attention(dim, num_heads)
         else:
             self.attn = WaveAttention(dim, num_heads, sr_ratio, wave)
-            
-        self.mlp = PVT2FFN(in_features=dim, hidden_features=int(dim * mlp_ratio))
+
+        if locality_ffn:
+            self.conv = LocalityFeedForward(dim, dim, 1, mlp_ratio, reduction=dim)
+        else:
+            self.norm2 = norm_layer(dim)
+            self.mlp = PVT2FFN(in_features=dim, hidden_features=int(dim * mlp_ratio))
+
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.apply(self._init_weights)
 
@@ -314,7 +438,16 @@ class Block(nn.Module):
 
     def forward(self, x, H, W):
         x = x + self.drop_path(self.attn(self.norm1(x), H, W))
-        x = x + self.drop_path(self.mlp(self.norm2(x), H, W))
+
+        if self.locality_ffn:
+            batch_size, num_token, embed_dim = x.shape  # (B, 197, dim)
+            patch_size = int(math.sqrt(num_token))
+
+            x = x.transpose(1, 2).view(batch_size, embed_dim, patch_size, patch_size)  # (B, dim, 14, 14)
+            x = self.conv(x).flatten(2).transpose(1, 2)  # (B, 196, dim)
+        else:
+            x = x + self.drop_path(self.mlp(self.norm2(x), H, W))
+
         return x
 
 class DownSamples(nn.Module):
@@ -409,6 +542,7 @@ class WaveViT(BaseModule):
         sr_ratios = [4, 2, 1, 1], 
         norm_layer = partial(nn.LayerNorm, eps=1e-6), 
         wave='haar',
+        locality_ffn=False,
         pretrained=None,
         **kwargs):
 
@@ -433,7 +567,8 @@ class WaveViT(BaseModule):
                 norm_layer=norm_layer,
                 sr_ratio=sr_ratios[i], 
                 block_type='wave' if i < 2 else 'std_att',
-                wave=wave)
+                wave=wave,
+                locality_ffn=locality_ffn)
             for j in range(depths[i])])
             
             norm = norm_layer(embed_dims[i])
