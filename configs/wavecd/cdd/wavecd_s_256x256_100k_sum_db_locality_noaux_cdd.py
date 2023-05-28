3184679842cd08b5d675ff4291f5acf54f7cbcf3
Fazry <lhuqita.fazry@gmail.com>
2023-05-29 03:31:28 +0700

    add new config

--- configs/wavecd/cdd/wavecd_s_256x256_100k_sum_db_locality_noaux_cdd.py
+++ configs/wavecd/cdd/wavecd_s_256x256_100k_sum_db_locality_noaux_cdd.py
@@ -42,8 +42,8 @@ train_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=32,
-    workers_per_gpu=8,
+    samples_per_gpu=16,
+    workers_per_gpu=4,
     train=dict(pipeline=train_pipeline)
 )
 
