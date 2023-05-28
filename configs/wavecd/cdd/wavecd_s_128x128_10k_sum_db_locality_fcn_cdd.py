240d168d8ce9f04e10c6c7eaa69b45e710176334
Fazry <lhuqita.fazry@gmail.com>
2023-05-28 15:54:23 +0700

    add new config

--- configs/wavecd/cdd/wavecd_s_128x128_10k_sum_db_locality_fcn_cdd.py
+++ configs/wavecd/cdd/wavecd_s_128x128_10k_sum_db_locality_fcn_cdd.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../_base_/models/siam_upernet_wavevit.py', '../../_base_/datasets/cdd.py',
+    '../../_base_/datasets/cdd.py',
     '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_10k.py'
 ]
 
