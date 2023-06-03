26a704e8eb92b0ea4e84932c83d8d5c1d8cbd15e
Fazry <lhuqita.fazry@gmail.com>
2023-06-04 03:50:03 +0700

    add new config

--- configs/wavecd/cdd/wavecd_s_256x256_50k_sum_locality_cdd.py
+++ configs/wavecd/cdd/wavecd_s_256x256_50k_sum_locality_cdd.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../../_base_/models/siam_upernet_wavevit.py', '../../_base_/datasets/cdd.py',
-    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_25k.py'
+    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_50k.py'
 ]
 
 embed_dims=[64, 128, 320, 448]
@@ -44,8 +44,8 @@ train_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=64,
-    workers_per_gpu=8,
+    samples_per_gpu=32,
+    workers_per_gpu=4,
     train=dict(pipeline=train_pipeline)
 )
 
@@ -66,4 +66,4 @@ lr_config = dict(_delete_=True, policy='poly',
 
 optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
 fp16 = dict()
-work_dir = './work_dirs/wavecd/cdd/wavecd_s_256x256_25k_sum_locality_cdd'
+work_dir = './work_dirs/wavecd/cdd/wavecd_s_256x256_50k_sum_locality_cdd'
