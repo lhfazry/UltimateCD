a886493770a565a994919fc1f7875777e47e12d2
Fazry <lhuqita.fazry@gmail.com>
2023-06-20 13:40:37 +0700

    add new config

--- tools/mcnemars.py
+++ tools/mcnemars.py
@@ -81,7 +81,7 @@ def perform_mcnemar_test(pred_folder1, pred_folder2, ground_truth, pred_prefix, 
             pred_path2 = os.path.join(pred_folder2, filename + pred_prefix)
 
             target_label = np.array(Image.open(gt_path).convert("L"))
-            pred1 = np.array(Image.open(pred_path1).resize((256, 256)))
+            pred1 = np.array(Image.open(pred_path1))
             pred2 = np.array(Image.open(pred_path2))
 
             #print(f"target_label: {target_label.shape}")
